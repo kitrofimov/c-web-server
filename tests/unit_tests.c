7f947dc6f8d456498001446ff65eb47863aa3a03
Kirill <kitrofimov39@gmail.com>
2023-07-01 12:21:14 +0500

    added CMakeLists.txt for unit tests

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -1,6 +1,7 @@
 // UNIT TESTS
 
 #include <stdio.h>
+#include <time.h>
 #include "../src/server_utils.h"
 #include "../lib/console_utils.h"
 
@@ -9,6 +10,8 @@
 int main(void)
 {
     int status;
+    struct timespec start, end;
+    double time_spent;
 
     // create_socket
     printf(WHT_BG BLK "CREATE_SOCKET\n" RESET);
@@ -25,7 +28,9 @@ int main(void)
     int backlog;
 
     for (int i = 0; i < NUM_TESTS_CREATE_SOCKET; i++)
-    {
+    { 
+        clock_gettime(CLOCK_REALTIME, &start);
+
         sprintf(port, "%i", create_socket_test_data[i][0]);
         backlog = create_socket_test_data[i][1];
         status = create_socket(port, backlog);
@@ -38,6 +43,11 @@ int main(void)
         {
             printf(RED_BG "FAIL" RESET " | Code: %i", status);
         }
-        printf(" | Create a socket at port %s with backlog of %i\n", port, backlog);
+
+        clock_gettime(CLOCK_REALTIME, &end);
+        time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+        printf(" | Time: %fs | Create a socket at port %s with backlog of %i\n",
+               time_spent, port, backlog);
     }
 }
